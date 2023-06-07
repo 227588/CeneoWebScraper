35e28991ff163691b759be435b5d552a7bbdf7f1
227588 <227588@student.uek.krakow.pl>
2023-06-07 12:57:59 +0200

    2023-06-07 WB

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Wojciech Bubula!"
+    return render_template('index.html')
 
-@app.route('/name/',defaults={'name': "Anonim"})
-@app.route("/name/<name>")
-def name(name=None):
-    return f"Hello {name}"
+@app.route('/extraction')
+def extraction():
+    return render_template('extraction.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/product_list')
+def product_list():
+    return render_template('product_list.html')
+
+@app.route('/author')
+def author():
+    return render_template('author.html')
+
+# @app.route('/name/',defaults={'name': "Anonim"})
+# @app.route("/name/<name>")
+# def name(name=None):
+#     return f"Hello {name}"
